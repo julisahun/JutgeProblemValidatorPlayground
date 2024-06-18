3fc9dfd1e45b86c069c762c41e5c39996a12aa5d
juli <julisahun@gmail.com>
2024-06-18 23:29:58 +0200

    add tailwindcss and generic components

--- main.js
+++ main.js
@@ -1,25 +1,87 @@
 import './style.css'
 import Validator from 'jutge-problem-validator'
+
+let showingOptions = {
+  recursive: false,
+  iterative: false
+}
+
+let options = {
+  programType: {},
+  libraries: {
+    forced: [],
+    prohibited: []
+  }
+}
+
+const [recCheckbox, iterCheckbox] = document.querySelectorAll('.checkbox')
+const [recSwitch, iterSwitch] = document.querySelectorAll('.switch')
+
+const [forcedLib, prohibitedLib] = document.querySelectorAll('.input')
+
 document.getElementById('main').addEventListener('click', async () => {
-    const file = document.getElementById('file').files[0]
-    if (!file) return alert('Please select a file')
-    const validator = new Validator()
-    validator.setOptions(getOptions())
-    const code = await file.text()
-    const result = await validator.validate(code)
-    document.getElementById('output').innerText = JSON.stringify(result, null, 2)
+  const file = document.getElementById('file').files[0]
+  if (!file) return alert('Please select a file')
+  const validator = new Validator()
+  validator.setOptions(options)
+  const code = await file.text()
+  const result = await validator.validate(code)
+  document.getElementById('result').innerText = JSON.stringify(result, null, 2)
 })
 
-function getOptions() {
-    const isRecursive = document.getElementById('recursive').checked
-    const isIterative = document.getElementById('iterative').checked
-    const useLib = document.getElementById('useLib').value.split(',').map(x => x.trim()).filter(Boolean)
-    const doNotUseLib = document.getElementById('noUseLib').value.split(',').map(x => x.trim()).filter(Boolean)
-    return {
-        programType: { recursive: isRecursive, iterative: isIterative }, libraries: {
-            forced: useLib,
-            prohibited: doNotUseLib
-        }
-    }
+recCheckbox.addEventListener('click', () => {
+  let state = recCheckbox.checked
+  showingOptions.recursive = state
+  if (state) options.programType.recursive = false
+  else {
+    delete options.programType.recursive
+    recSwitch.checked = false
+  }
+
+  recSwitch.style.visibility = state ? 'visible' : 'hidden'
+  setOptions()
+})
+
+iterCheckbox.addEventListener('click', (e) => {
+  let state = iterCheckbox.checked
+  showingOptions.iterative = state
+  if (state) options.programType.iterative = false
+  else {
+    delete options.programType.iterative
+    iterSwitch.checked = false
+  }
+  iterSwitch.style.visibility = state ? 'visible' : 'hidden'
+  setOptions()
+})
+
+recSwitch.addEventListener('click', () => {
+  let state = recSwitch.checked
+  options.programType.recursive = state
+  setOptions()
+})
+
+iterSwitch.addEventListener('click', () => {
+  let state = iterSwitch.checked
+  options.programType.iterative = state
+  setOptions()
+})
+
+forcedLib.addEventListener('input', (e) => {
+  options.libraries.forced = e.target.value.split(',').map(lib => lib.trim())
+  setOptions()
+})
+
+prohibitedLib.addEventListener('input', (e) => {
+  options.libraries.prohibited = e.target.value.split(',').map(lib => lib.trim())
+  setOptions()
+})
+
+
+
+
+function setOptions() {
+  document.getElementById('options').innerText = JSON.stringify(options, null, 2)
 }
+
+setOptions()
 
